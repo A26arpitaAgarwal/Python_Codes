e33645c9dafc3c5cc151361c4d3e3e19d50aabc8
Arpita Agarwal <120187471+A26arpitaAgarwal@users.noreply.github.com>
2024-01-08 19:40:29 +0530

    Create Factors.py

---
+++ Factors.py
@@ -0,0 +1,10 @@
+#finding factors of a number
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = int(input("enter a number: "))
+
+print_factors(num)
