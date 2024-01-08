82af55ec4c76e655e12033b53d7aa2916ce10817
Arpita Agarwal <120187471+A26arpitaAgarwal@users.noreply.github.com>
2024-01-08 19:35:15 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,12 @@
+#find factorial of a number
+num = int(input("enter a number: "))
+factorial = 1
+
+if num<0:
+    print("The factorial doesn't exist.")
+elif num==0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        factorial = factorial*i
+    print("The factorial of ",num,"is",factorial)
