e66ea873adae537289604b7b1a1a0718b2ca4386
Arpita Agarwal <120187471+A26arpitaAgarwal@users.noreply.github.com>
2024-01-08 19:45:58 +0530

    Create swapping.py

---
+++ swapping.py
@@ -0,0 +1,15 @@
+#swaping the last element with first
+def swapList(newList):
+    size = len(newList)
+     
+    # Swapping
+    temp = newList[0]
+    newList[0] = newList[size - 1]
+    newList[size - 1] = temp
+     
+    return newList
+     
+# Driver code
+newList = [12, 35, 9, 56, 24]
+ 
+print(swapList(newList))
