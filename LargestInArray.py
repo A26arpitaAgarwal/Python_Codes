d6b05259898b9c5dc2ec3faf893bfd2eb2035ce4
Arpita Agarwal <120187471+A26arpitaAgarwal@users.noreply.github.com>
2024-01-08 19:39:07 +0530

    Create LargestInArray.py

---
+++ LargestInArray.py
@@ -0,0 +1,11 @@
+#finding the largest element in an array
+def largest(arr, n):
+    max = arr[0]
+    for i in range(1, n):
+        if arr[i] > max:
+            max = arr[i]
+    return max
+arr = [10, 324, 45, 90, 9808]
+n = len(arr)
+Ans = largest(arr, n)
+print("Largest in given array ", Ans)
